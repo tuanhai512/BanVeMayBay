0fbe8aa6ca65e8de11bc29c879118190ffdfad48
sugar2507 <72238592+sugar2507@users.noreply.github.com>
2021-05-06 08:54:12 +0700

    updata

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Reports from './pages/Reports';
-import Reports1 from './pages/Reports1';
 
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
           <Route path='/' exact component={Home}></Route>
           <Route path='/product' exact component={Products}></Route>
           <Route path='/reports' exact component={Reports}></Route>
-          <Route path='/reports1' exact component={Reports1}></Route>
         </Switch>
       </Router>  
     </>
